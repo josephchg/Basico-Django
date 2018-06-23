166df5212f2dc697677221954c74205a2860e766
josephchg <josephchg25@gmail.com>
2018-06-22 20:27:32 -0500

    Cambios en el modelo

--- mascota/models.py
+++ mascota/models.py
@@ -10,13 +10,12 @@ class Vacuna(models.Model):
         return self.nombre
 
 class Mascota(models.Model):
-    folio=models.CharField(max_length=10,primary_key=True)
     nombre=models.CharField(max_length=50)
     sexo=models.CharField(max_length=10)
     edad_aproximada=models.IntegerField()
     fecha_rescate=models.DateField()
     persona=models.ForeignKey(Persona,null=True,blank=True,on_delete=models.CASCADE)
-    vacuna=models.ManyToManyField(Vacuna)
+    vacuna=models.ManyToManyField(Vacuna,blank=True)
 
     def __str__(self):
         return self.nombre
